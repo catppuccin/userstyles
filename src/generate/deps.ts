84db29193074f512b71a8a66bb4ef484340eec0c
rubyowo <perhaps-you-know@what-is.ml>
2023-03-23 10:25:58 +0400

    refactor: auto generate CI files & remove bloat (#9)

--- src/generate/deps.ts
+++ src/generate/deps.ts
@@ -2,6 +2,8 @@ export { parse as parseYaml } from "https://deno.land/std@0.172.0/encoding/yaml.
 
 import Ajv from "npm:ajv@8.12.0";
 import * as path from "https://deno.land/std@0.172.0/path/mod.ts";
-import schema from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.schema.json" assert { type: "json" };
+import schema from "../userstyles.schema.json" assert { type: "json" };
+import portsSchema from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.schema.json" assert { type: "json" };
 
-export { Ajv, path, schema };
+export type { Categories as PortCategories } from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/generate/types.d.ts";
+export { Ajv, path, portsSchema, schema };
