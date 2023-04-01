764c52c6d7439c1cb465c9790abd1dfc0f6d327a
rubyowo <perhaps-you-know@what-is.ml>
2023-04-01 08:35:35 +0400

    feat: generate readmes in the main readme only

--- src/generate/deps.ts
+++ src/generate/deps.ts
@@ -1,9 +1,10 @@
-export { parse as parseYaml } from "https://deno.land/std@0.172.0/encoding/yaml.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.181.0/encoding/yaml.ts";
 
 import Ajv from "npm:ajv@8.12.0";
-import * as path from "https://deno.land/std@0.172.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.181.0/path/mod.ts";
+import { walkSync } from "https://deno.land/std@0.181.0/fs/walk.ts";
 import schema from "../userstyles.schema.json" assert { type: "json" };
 import portsSchema from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.schema.json" assert { type: "json" };
 
 export type { Categories as PortCategories } from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/generate/types.d.ts";
-export { Ajv, path, portsSchema, schema };
+export { Ajv, path, walkSync, portsSchema, schema };
