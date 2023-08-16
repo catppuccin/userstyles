12b1bd33e79c6664eb96bc628c98863cc16147c0
rubyowo <perhaps-you-know@what-is.ml>
2023-08-16 14:19:40 +0400

    formatting

--- src/generate/syncMaintainers.ts
+++ src/generate/syncMaintainers.ts
@@ -10,34 +10,48 @@ type Metadata = {
 const ajv = new (Ajv as unknown as (typeof Ajv)["default"])();
 const validate = ajv.compile<Metadata>(schema);
 
-const userstylesYaml = Deno.readTextFileSync(
-  path.join(ROOT, "../userstyles.yml"),
-);
+const userstylesYaml = Deno.readTextFileSync(path.join(ROOT, "../userstyles.yml"));
 const userstylesData = parseYaml(userstylesYaml);
 if (!validate(userstylesData)) {
   console.error(validate.errors);
   Deno.exit(1);
 }
 
-const maintainers = [...new Set(Object.values(userstylesData.userstyles).flatMap((style) =>
-  style.readme["current-maintainers"].map((m) => m.url.split("/").pop().toLowerCase())
-))];
+const maintainers = [
+  ...new Set(
+    Object.values(userstylesData.userstyles).flatMap((style) =>
+      style.readme["current-maintainers"].map((m) =>
+        m.url.split("/").pop().toLowerCase()
+      )
+    )
+  )
+];
 
-const requestGH = async (endpoint: string, method = "GET", returnJson = true, body?: string) => {
-  const res = await fetch("https://api.github.com/orgs/catppuccin/teams/userstyles-maintainers" + endpoint, {
-    method,
-    headers: {
-      "Authorization": `Bearer ${Deno.args[0]}`
+const requestGH = async (
+  endpoint: string,
+  method = "GET",
+  returnJson = true,
+  body?: string,
+) => {
+  const res = await fetch(
+    "https://api.github.com/orgs/catppuccin/teams/userstyles-maintainers" +
+    endpoint,
+    {
+      method,
+      headers: {
+        "Authorization": `Bearer ${Deno.args[0]}`,
+      },
+      body,
     },
-    body,
-  })
-  return returnJson ? res.json() : res
-}
+  );
+  return returnJson ? res.json() : res;
+};
 
-const teamMembers = (await requestGH("/members")).map(l => l.login.toLowerCase());
+const teamMembers = (await requestGH("/members")).map((l: { login: string; }) => l.login.toLowerCase());
 
-const checkEquality = (a, b) => a.length === b.length && a.every((e: string) => b.includes(e));
-const getExtra = (a, b) => a.filter((e: string) => !b.includes(e))
+const checkEquality = (a: string[], b: string[]) =>
+  a.length === b.length && a.every((e) => b.includes(e));
+const getExtra = (a: string[], b: string[]) => a.filter((e) => !b.includes(e));
 
 const syncMaintainers = async () => {
   if (checkEquality(maintainers, teamMembers)) {
@@ -46,14 +60,19 @@ const syncMaintainers = async () => {
   }
   if (maintainers.length > teamMembers.length) {
     for (const m of getExtra(maintainers, teamMembers)) {
-      await requestGH(`/memberships/${m}`, 'PUT', false, JSON.stringify({ role: "member" }))
-      console.log(`Added ${m} to the team`)
+      await requestGH(
+        `/memberships/${m}`,
+        "PUT",
+        false,
+        JSON.stringify({ role: "member" }),
+      );
+      console.log(`Added ${m} to the team`);
     }
   } else {
     for (const m of getExtra(teamMembers, maintainers)) {
-      await requestGH(`/memberships/${m}`, 'DELETE', false)
-      console.log(`Removed ${m} from the team`)
+      await requestGH(`/memberships/${m}`, "DELETE", false);
+      console.log(`Removed ${m} from the team`);
     }
   }
-}
-await syncMaintainers()
+};
+await syncMaintainers();
