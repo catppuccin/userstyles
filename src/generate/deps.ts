b74bb16f187f84ab6a871df3dfe16bf00173f285
rubyowo <perhaps-you-know@what-is.ml>
2023-08-20 13:42:35 +0400

    ci: automatically sync maintainers in the team (#174)
    Co-authored-by: winston <hey@winston.sh

--- src/generate/deps.ts
+++ src/generate/deps.ts
@@ -1,10 +1,15 @@
-export { parse as parseYaml } from "https://deno.land/std@0.181.0/encoding/yaml.ts";
+export type { Categories as PortCategories } from "https://raw.githubusercontent.com/catppuccin/catppuccin/cad3fe6c9eb2476f9787c386a6b9c70de8e6d468/resources/generate/types.d.ts";
+
+export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
+export * as assert from "https://deno.land/std@0.198.0/assert/mod.ts";
+export { walkSync } from "https://deno.land/std@0.198.0/fs/walk.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.198.0/yaml/parse.ts";
+export * from "npm:@octokit/rest";
 
 import Ajv from "npm:ajv@8.12.0";
-import * as path from "https://deno.land/std@0.181.0/path/mod.ts";
-import { walkSync } from "https://deno.land/std@0.181.0/fs/walk.ts";
 import schema from "../userstyles.schema.json" assert { type: "json" };
-import portsSchema from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/ports.schema.json" assert { type: "json" };
+import portsSchema from "https://raw.githubusercontent.com/catppuccin/catppuccin/cad3fe6c9eb2476f9787c386a6b9c70de8e6d468/resources/ports.schema.json" assert {
+  type: "json",
+};
 
-export type { Categories as PortCategories } from "https://raw.githubusercontent.com/catppuccin/catppuccin/main/resources/generate/types.d.ts";
-export { Ajv, path, portsSchema, schema, walkSync };
+export { Ajv, portsSchema, schema };
