613cd9ae6eded13e410c7b3a8d469cff4fb88bd9
winston <hey@winston.sh>
2023-11-15 05:14:41 +0100

    ci(lint): bump

--- src/lint/main.ts
+++ src/lint/main.ts
@@ -3,10 +3,10 @@
 // TODO: remove this once types for usercss-meta are available
 // deno-lint-ignore-file no-explicit-any
 
-import { basename, dirname, join } from "std/path/mod.ts";
+import { basename, dirname, join, relative } from "std/path/mod.ts";
 import { globber } from "globber";
 
-import core, { summary } from "@actions/core";
+import core from "@actions/core";
 // @deno-types="npm:@types/less";
 import less from "less";
 import usercssMeta from "usercss-meta";
@@ -18,7 +18,7 @@ const iterator = globber({
   cwd: REPO_ROOT,
 });
 
-let total_missing_files = 0;
+const missingFiles: string[] = [];
 
 const assertions = (repo: string) => {
   const pfx = "https://github.com/catppuccin/userstyles";
@@ -94,31 +94,21 @@ for await (const entry of iterator) {
     });
   });
 
-  const missing_files: string[] = [];
   [
     "catppuccin.user.css",
     ...["latte", "frappe", "macchiato", "mocha", "catwalk"].map((f) =>
       `assets/${f}.webp`
     ),
   ].map(async (fp) => {
+    const filepath = join(repodir, fp);
     await Deno.stat(join(repodir, fp)).catch(() => {
-      missing_files.push(fp);
-      total_missing_files++;
+      missingFiles.push(relative(REPO_ROOT, filepath));
     });
   });
-
-  if (missing_files.length !== 0) {
-    summary.addHeading("Missing Files");
-    summary.addList(missing_files, true);
-  }
 }
 
 // only write summary if running in github actions
-if (Deno.env.has("GITHUB_STEP_SUMMARY")) {
-  await summary.write();
-}
-
-// missing files are a fatal error
-if (total_missing_files !== 0) {
+if (Deno.env.has("GITHUB_ACTIONS") && missingFiles.length !== 0) {
+  await core.summary.addHeading("Missing files").addList(missingFiles).write();
   Deno.exit(1);
 }
