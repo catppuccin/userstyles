ff8da1e094a42718501e7615d03f06a7cf680001
winston <hey@winston.sh>
2023-11-16 22:45:25 +0100

    ci(lint): init (#318)
    * ci(lint): init
    
    This finishes what #132 started, supersedes #317 (thanks for the stylelint setup!)
    
    * ci(lint): add stylelint
    
    Co-authored-by: uncenter <47499684+uncenter@users.noreply.github.com>

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,2 @@
-export * from "../types/userstyles.d.ts";
+export * from "./types/userstyles.d.ts";
 export type { Categories as PortCategories } from "catppuccin-repo/resources/generate/types.d.ts";
