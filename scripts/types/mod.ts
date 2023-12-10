1e98253435cffa393ae6e67e9644abe1f149df54
Hamothy <58985301+sgoudham@users.noreply.github.com>
2023-12-10 17:28:16 +0000

    build: fix path & bump schema (#381)

--- scripts/types/mod.ts
+++ scripts/types/mod.ts
@@ -1,5 +1,5 @@
 export * as UserStylesSchema from "@/types/userstyles.d.ts";
-export * as PortsSchema from "catppuccin-repo/resources/generate/types.d.ts";
+export * as PortsSchema from "catppuccin-repo/resources/types/ports.d.ts";
 
 import { Entries } from "https://esm.sh/type-fest@4.8.1";
 declare global {
